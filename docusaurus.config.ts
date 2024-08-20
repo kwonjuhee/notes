1c9906a445bc54712ca742295270596749c8c3d7
kwonjuhee <77ooee@gmail.com>
2024-08-20 11:25:59 +0900

    add gtag

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -38,6 +38,10 @@ const config: Config = {
         theme: {
           customCss: "./src/css/custom.css",
         },
+        gtag: {
+          trackingID: "G-T5DM2XSS7G",
+          anonymizeIP: true,
+        },
       } satisfies Preset.Options,
     ],
   ],
