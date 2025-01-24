4a0fc8bc51d115f66b2e10f4066f247dbe9b3ce4
kwonjuhee <77ooee@gmail.com>
2025-01-24 20:57:34 +0900

    refactor: extract common layout props from Box

--- components/Box/Box.tsx
+++ components/Box/Box.tsx
@@ -1,51 +1,10 @@
 import clsx from "clsx";
+import { getLayoutCustomProperties, LayoutProps } from "@/types/layout";
 import { Color, Radius } from "@/types/token";
 import styles from "./Box.module.css";
 
 type Display = "none" | "inline" | "block";
-type MarginProps = {
-  margin?: string;
-  marginX?: string;
-  marginY?: string;
-  marginTop?: string;
-  marginRight?: string;
-  marginBottom?: string;
-  marginLeft?: string;
-};
-type PaddingProps = {
-  padding?: string;
-  paddingX?: string;
-  paddingY?: string;
-  paddingTop?: string;
-  paddingRight?: string;
-  paddingBottom?: string;
-  paddingLeft?: string;
-};
-type Overflow = "visible" | "hidden" | "clip" | "scroll" | "auto";
-type WidthAndHeightProps = {
-  width?: string;
-  minWidth?: string;
-  maxWidth?: string;
-  height?: string;
-  minHeight?: string;
-  maxHeight?: string;
-  overflow?: Overflow;
-  overflowX?: Overflow;
-  overflowY?: Overflow;
-};
-type Position = "static" | "relative" | "absolute" | "fixed" | "sticky";
-type PositionProps = {
-  position?: Position;
-  top?: string;
-  right?: string;
-  bottom?: string;
-  left?: string;
-};
-type FlexItemProps = {
-  flexBasis?: "auto" | "max-content" | "min-content" | "fit-content" | "content" | string;
-  flexGrow?: number;
-  flexShrink?: number;
-};
+
 type StyleProps = {
   backgroundColor?: Color;
   borderWidth?: string;
@@ -57,23 +16,18 @@ type StyleProps = {
   borderRadius?: Radius;
 };
 
-export interface BoxProps
-  extends React.ComponentPropsWithoutRef<"div">,
-    MarginProps,
-    PaddingProps,
-    WidthAndHeightProps,
-    PositionProps,
-    FlexItemProps,
-    StyleProps {
+export interface BoxProps extends React.ComponentPropsWithoutRef<"div">, LayoutProps, StyleProps {
   display?: Display;
 }
 
 export const Box = ({ display = "block", style, className, children, ...props }: BoxProps) => {
-  const { boxCustomProperties, boxProps } = getBoxCustomProperties(props);
+  const { layoutCustomProperties, restProps } = getLayoutCustomProperties(props);
+  const { boxCustomProperties, boxProps } = getBoxCustomProperties(restProps);
 
   return (
     <div
       style={{
+        ...layoutCustomProperties,
         ...boxCustomProperties,
         ...style,
       }}
@@ -87,34 +41,6 @@ export const Box = ({ display = "block", style, className, children, ...props }:
 
 const getBoxCustomProperties = (props: BoxProps) => {
   const {
-    margin,
-    marginX,
-    marginY,
-    marginTop,
-    marginRight,
-    marginBottom,
-    marginLeft,
-    padding,
-    paddingX,
-    paddingY,
-    paddingTop,
-    paddingRight,
-    paddingBottom,
-    paddingLeft,
-    width,
-    minWidth,
-    maxWidth,
-    height,
-    minHeight,
-    maxHeight,
-    position,
-    top,
-    right,
-    bottom,
-    left,
-    flexBasis,
-    flexGrow,
-    flexShrink,
     backgroundColor,
     borderWidth,
     borderTopWidth,
@@ -127,42 +53,14 @@ const getBoxCustomProperties = (props: BoxProps) => {
   } = props;
   return {
     boxCustomProperties: {
-      "--box-margin": margin,
-      "--box-margin-left": marginLeft,
-      "--box-margin-x": marginX,
-      "--box-margin-y": marginY,
-      "--box-margin-top": marginTop,
-      "--box-margin-right": marginRight,
-      "--box-margin-bottom": marginBottom,
-      "--box-padding": padding,
-      "--box-padding-x": paddingX,
-      "--box-padding-y": paddingY,
-      "--box-padding-top": paddingTop,
-      "--box-padding-right": paddingRight,
-      "--box-padding-bottom": paddingBottom,
-      "--box-padding-left": paddingLeft,
-      "--box-width": width,
-      "--box-min-width": minWidth,
-      "--box-max-width": maxWidth,
-      "--box-height": height,
-      "--box-min-height": minHeight,
-      "--box-max-height": maxHeight,
-      "--box-position": position,
-      "--box-top": top,
-      "--box-right": right,
-      "--box-bottom": bottom,
-      "--box-left": left,
-      "--box-flex-basis": flexBasis,
-      "--box-flex-grow": flexGrow,
-      "--box-flex-shrink": flexShrink,
-      "--box-background-color": backgroundColor ? `var(--bg-${backgroundColor}-subtle)` : undefined,
-      "--box-border-width": borderWidth,
-      "--box-border-top-width": borderTopWidth,
-      "--box-border-right-width": borderRightWidth,
-      "--box-border-bottom-width": borderBottomWidth,
-      "--box-border-left-width": borderLeftWidth,
-      "--box-border-color": borderColor ? `var(--border-${borderColor})` : undefined,
-      "--box-border-radius": borderRadius ? `var(--radius-${borderRadius})` : undefined,
+      "--background-color": backgroundColor ? `var(--bg-${backgroundColor}-subtle)` : undefined,
+      "--border-width": borderWidth,
+      "--border-top-width": borderTopWidth,
+      "--border-right-width": borderRightWidth,
+      "--border-bottom-width": borderBottomWidth,
+      "--border-left-width": borderLeftWidth,
+      "--border-color": borderColor ? `var(--border-${borderColor})` : undefined,
+      "--border-radius": borderRadius ? `var(--radius-${borderRadius})` : undefined,
     },
     boxProps: rest,
   };
---
+++ types/layout.ts
@@ -0,0 +1,122 @@
+export type MarginProps = {
+  margin?: string;
+  marginX?: string;
+  marginY?: string;
+  marginTop?: string;
+  marginRight?: string;
+  marginBottom?: string;
+  marginLeft?: string;
+};
+
+export type PaddingProps = {
+  padding?: string;
+  paddingX?: string;
+  paddingY?: string;
+  paddingTop?: string;
+  paddingRight?: string;
+  paddingBottom?: string;
+  paddingLeft?: string;
+};
+
+export type Overflow = "visible" | "hidden" | "clip" | "scroll" | "auto";
+
+export type WidthAndHeightProps = {
+  width?: string;
+  minWidth?: string;
+  maxWidth?: string;
+  height?: string;
+  minHeight?: string;
+  maxHeight?: string;
+  overflow?: Overflow;
+  overflowX?: Overflow;
+  overflowY?: Overflow;
+};
+
+export type Position = "static" | "relative" | "absolute" | "fixed" | "sticky";
+
+export type PositionProps = {
+  position?: Position;
+  top?: string;
+  right?: string;
+  bottom?: string;
+  left?: string;
+};
+
+export type FlexItemProps = {
+  flexBasis?: "auto" | "max-content" | "min-content" | "fit-content" | "content" | string;
+  flexGrow?: number;
+  flexShrink?: number;
+};
+
+export type LayoutProps = MarginProps &
+  PaddingProps &
+  WidthAndHeightProps &
+  PositionProps &
+  FlexItemProps;
+
+export const getLayoutCustomProperties = (props: LayoutProps) => {
+  const {
+    margin,
+    marginX,
+    marginY,
+    marginTop,
+    marginRight,
+    marginBottom,
+    marginLeft,
+    padding,
+    paddingX,
+    paddingY,
+    paddingTop,
+    paddingRight,
+    paddingBottom,
+    paddingLeft,
+    width,
+    minWidth,
+    maxWidth,
+    height,
+    minHeight,
+    maxHeight,
+    position,
+    top,
+    right,
+    bottom,
+    left,
+    flexBasis,
+    flexGrow,
+    flexShrink,
+    ...rest
+  } = props;
+  return {
+    layoutCustomProperties: {
+      "--margin": margin,
+      "--margin-left": marginLeft,
+      "--margin-x": marginX,
+      "--margin-y": marginY,
+      "--margin-top": marginTop,
+      "--margin-right": marginRight,
+      "--margin-bottom": marginBottom,
+      "--padding": padding,
+      "--padding-x": paddingX,
+      "--padding-y": paddingY,
+      "--padding-top": paddingTop,
+      "--padding-right": paddingRight,
+      "--padding-bottom": paddingBottom,
+      "--padding-left": paddingLeft,
+      "--width": width,
+      "--min-width": minWidth,
+      "--max-width": maxWidth,
+      "--height": height,
+      "--min-height": minHeight,
+      "--max-height": maxHeight,
+      "--position": position,
+      "--top": top,
+      "--right": right,
+      "--bottom": bottom,
+      "--left": left,
+      "--flex-basis": flexBasis,
+      "--flex-grow": flexGrow,
+      "--flex-shrink": flexShrink,
+    },
+    restProps: rest,
+  };
+};
