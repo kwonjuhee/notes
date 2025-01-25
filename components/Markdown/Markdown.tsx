3743973163eb9f3c4e1aa5c0fe0aeeff17aeced4
kwonjuhee <77ooee@gmail.com>
2025-01-25 23:43:51 +0900

    style: add markdown style

--- components/Markdown/Markdown.tsx
+++ components/Markdown/Markdown.tsx
@@ -2,7 +2,26 @@ import { MDXRemote, MDXRemoteProps } from "next-mdx-remote/rsc";
 import remarkBreaks from "remark-breaks";
 import remarkGfm from "remark-gfm";
 import remarkWikiLink from "remark-wiki-link";
+import { Text } from "../Text";
 import styles from "./Markdown.module.css";
+
+const components: MDXRemoteProps["components"] = {
+  h1: (props) => (
+    <Text as="h1" variant="heading30" {...props}>
+      {props.children}
+    </Text>
+  ),
+  h2: (props) => (
+    <Text as="h2" variant="heading24" {...props}>
+      {props.children}
+    </Text>
+  ),
+  h3: (props) => (
+    <Text as="h3" variant="heading20" {...props}>
+      {props.children}
+    </Text>
+  ),
+};
 
 export const Markdown = async ({ source, ...props }: MDXRemoteProps) => {
   return (
@@ -27,7 +46,7 @@ export const Markdown = async ({ source, ...props }: MDXRemoteProps) => {
           },
         }}
         {...props}
-        components={{ ...(props.components ?? {}) }}
+        components={{ ...components, ...(props.components ?? {}) }}
       />
     </div>
   );
