15a43900c46b91c5dbf459baf7bb11bd106e2860
kwonjuhee <77ooee@gmail.com>
2025-02-05 23:34:48 +0900

    feat: implement Input component

---
+++ components/Input/Input.stories.tsx
@@ -0,0 +1,33 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { useState } from "react";
+import { Input } from "./Input";
+
+const meta: Meta<typeof Input> = {
+  component: Input,
+};
+export default meta;
+
+export const Primary: StoryObj<typeof Input> = {
+  args: {
+    placeholder: "placeholder",
+  },
+};
+
+export const Controlled: StoryObj<typeof Input> = {
+  render: (args) => {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
+    const [value, setValue] = useState("");
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setValue(e.target.value);
+    };
+
+    return <Input value={value} onChange={onChange} {...args} />;
+  },
+};
+
+export const Disabled: StoryObj<typeof Input> = {
+  args: {
+    disabled: true,
+  },
+};
---
+++ components/Input/Input.tsx
@@ -0,0 +1,22 @@
+import clsx from "clsx";
+import { forwardRef } from "react";
+import styles from "./Input.module.css";
+
+export interface InputProps extends React.ComponentPropsWithRef<"input"> {
+  leftContent: React.ReactNode;
+  rightContent: React.ReactNode;
+}
+
+export const Input = forwardRef<HTMLInputElement, InputProps>(
+  ({ leftContent, rightContent, className, ...props }, ref) => {
+    return (
+      <div className={clsx(styles.Input, className)}>
+        {leftContent}
+        <input ref={ref} className={styles.input} {...props} />
+        {rightContent}
+      </div>
+    );
+  }
+);
+
+Input.displayName = "Input";
