9b8d1c00d531a53d3d71fd6114489a2415254a07
kwonjuhee <77ooee@gmail.com>
2025-02-06 00:27:32 +0900

    feat: implement SearchItem component

---
+++ app/wiki/components/SearchItem/SearchItem.stories.tsx
@@ -0,0 +1,16 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { SearchItem } from "./SearchItem";
+
+const meta: Meta<typeof SearchItem> = {
+  title: "wiki/SearchItem",
+  component: SearchItem,
+};
+export default meta;
+
+export const Primary: StoryObj<typeof SearchItem> = {
+  args: {
+    title: "This is Title",
+    category: "Category",
+    highlightKeyword: "Title",
+  },
+};
---
+++ app/wiki/components/SearchItem/SearchItem.tsx
@@ -0,0 +1,31 @@
+import { Text } from "@/components/Text";
+import styles from "./SearchItem.module.css";
+
+export interface SearchItemProps {
+  title: string;
+  category: string;
+  highlightKeyword?: string;
+}
+
+export const SearchItem = ({ title, category, highlightKeyword }: SearchItemProps) => {
+  const parts = title.split(new RegExp(`(${highlightKeyword})`, "gi"));
+
+  return (
+    <div className={styles.SearchItem}>
+      <Text variant="body16" className={styles.title}>
+        {parts.map((part, i) =>
+          part === highlightKeyword ? (
+            <em key={i} className={styles.highlight}>
+              {part}
+            </em>
+          ) : (
+            part
+          )
+        )}
+      </Text>
+      <Text variant="caption12" className={styles.category}>
+        {category}
+      </Text>
+    </div>
+  );
+};
