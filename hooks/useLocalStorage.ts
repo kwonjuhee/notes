21d85b2f147b6159b57a4865a8d3b79174ed49b0
kwonjuhee <77ooee@gmail.com>
2025-02-13 01:25:32 +0900

    feat: implement useLocalStorage hook

---
+++ hooks/useLocalStorage.ts
@@ -0,0 +1,31 @@
+import { useSyncExternalStore } from "react";
+
+export const useLocalStorage = <T extends string>(key: string, initialValue?: T) => {
+  const getSnapshot = () => {
+    const value = window.localStorage.getItem(key);
+    return value ? JSON.parse(value) : initialValue;
+  };
+
+  const store = useSyncExternalStore(
+    (callback) => {
+      window.addEventListener("storage", callback);
+      return () => {
+        window.addEventListener("storate", callback);
+      };
+    },
+    getSnapshot,
+    () => initialValue
+  );
+
+  const setItem = (value: T) => {
+    window.localStorage.setItem(key, JSON.stringify(value));
+    window.dispatchEvent(new Event("storage"));
+  };
+
+  const removeItem = () => {
+    window.localStorage.removeItem(key);
+    window.dispatchEvent(new Event("storage"));
+  };
+
+  return { value: store, set: setItem, remove: removeItem };
+};
