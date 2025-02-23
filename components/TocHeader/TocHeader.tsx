952cf433784e169423fb9dc4112b3f8e4a252370
kwonjuhee <77ooee@gmail.com>
2025-02-23 21:36:21 +0900

    feat: display current heading on TocHeader

--- components/TocHeader/TocHeader.tsx
+++ components/TocHeader/TocHeader.tsx
@@ -1,14 +1,16 @@
 "use client";
 
 import { useState } from "react";
-import { List } from "@/assets/icon";
+import { CaretRight, List } from "@/assets/icon";
 import { Box } from "../Box";
 import { Collapsible } from "../Collapsible";
 import { TableOfContents } from "../TableOfContents";
+import { useBuildToc } from "../TableOfContents/useBuildToc";
 import { Text } from "../Text";
 import styles from "./TocHeader.module.css";
 
 export const TocHeader = () => {
+  const { currentItem } = useBuildToc();
   const [isOpen, setIsOpen] = useState(false);
 
   return (
@@ -19,8 +21,10 @@ export const TocHeader = () => {
     >
       <Collapsible.Trigger className={styles.trigger}>
         <List className={styles.listIcon} width={20} height={20} />
+        <Text variant="caption14">On this page</Text>
+        <CaretRight width={13} height={13} />
         <Text variant="caption14" truncate>
-          On this page
+          {currentItem?.text}
         </Text>
       </Collapsible.Trigger>
       <Collapsible.Content className={styles.content}>
