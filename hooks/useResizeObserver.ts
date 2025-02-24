db2bcbd8a71fe5e793841919407360114bf473da
kwonjuhee <77ooee@gmail.com>
2025-02-24 14:28:06 +0900

    feat: implement useResizeObserver hook

---
+++ hooks/useResizeObserver.ts
@@ -0,0 +1,39 @@
+import { useEffect, useRef, useState } from "react";
+
+type Size = {
+  width: number | undefined;
+  height: number | undefined;
+};
+
+type UseResizeObserverProps<T> = {
+  ref: React.RefObject<T>;
+  onResize?: (size: Size) => void;
+};
+
+export const useResizeObserver = <T extends HTMLElement>({
+  ref,
+  onResize: _onResize,
+}: UseResizeObserverProps<T>) => {
+  const [size, setSize] = useState<Size>({ width: undefined, height: undefined });
+  const onResize = useRef<((size: Size) => void) | undefined>(_onResize);
+
+  useEffect(() => {
+    const element = ref.current;
+    if (!element) return;
+
+    const observer = new ResizeObserver(([entry]) => {
+      const width = entry.borderBoxSize[0].inlineSize;
+      const height = entry.borderBoxSize[0].blockSize;
+      setSize({ width, height });
+      onResize.current?.({ width, height });
+    });
+
+    observer.observe(element, { box: "border-box" });
+
+    return () => {
+      observer.unobserve(element);
+    };
+  }, [ref]);
+
+  return size;
+};
