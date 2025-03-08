9a0df768d7ca990c257df1d8432c40b1bad39fbe
kwonjuhee <77ooee@gmail.com>
2025-03-08 16:21:46 +0900

    fix: filter private notes by auth status

--- api/note.ts
+++ api/note.ts
@@ -1,4 +1,6 @@
 import { notFound } from "next/navigation";
+import { cache } from "react";
+import { checkAuthentication } from "@/lib/auth";
 import { Category } from "@/types/category";
 import { Note } from "@/types/note";
 import { decodeBase64 } from "@/utils/endecoder";
@@ -6,20 +8,33 @@ import { isMarkdownFile, isPrivatePath } from "@/utils/markdown";
 import { githubApi } from "./github";
 import { Blob, GetFileContentResponseData } from "./github.types";
 
-const getNoteList = async (): Promise<Note[]> => {
+const getNoteList = cache(async (): Promise<Note[]> => {
   const { ref } = await githubApi.gitDatabase.getRef();
-  const { tree } = await githubApi.gitDatabase.getGitTree(ref);
+  const [{ tree }, categoryList] = await Promise.all([
+    await githubApi.gitDatabase.getGitTree(ref),
+    getCategoryList(),
+  ]);
 
   const blobs = tree.filter((node) => node.type === "blob") as Blob[];
-
   const notes = blobs.map(transformBlobToNote);
 
+  const { isLoggedIn } = await checkAuthentication();
+
+  const isExistCategoryPath = (path: string) =>
+    categoryList.some((category) => path.startsWith(category.slug));
+
+  const isPublicCategoryPath = (path: string) =>
+    categoryList.some((category) => path.startsWith(category.slug) && !category.isPrivate);
+
   const filteredNotes = notes.filter(
-    (note) => !isPrivatePath(note.path) && isMarkdownFile(note.path)
+    (note) =>
+      !isPrivatePath(note.path) &&
+      isMarkdownFile(note.path) &&
+      (isLoggedIn ? isExistCategoryPath(note.path) : isPublicCategoryPath(note.path))
   );
 
   return filteredNotes;
-};
+});
 
 const transformBlobToNote = (blob: Blob): Note => {
   const slugs = blob.path.split("/");
@@ -34,11 +49,6 @@ const getNotesByCategory = async (categorySlug: string) => {
   const category = categoryList.find(({ slug }) => slug === categorySlug);
   if (!category) {
     return notFound();
-  }
-
-  if (category?.isPrivate) {
-    // @TODO check authentication
-    return [];
   }
 
   return notes.filter((note) => note.path.startsWith(categorySlug));
