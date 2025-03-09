928e60f1ccdfda2f082aa786173a273a29ba60a5
kwonjuhee <77ooee@gmail.com>
2025-03-10 00:10:25 +0900

    chore: add logging config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  logging: {
+    fetches: {
+      fullUrl: true,
+    },
+  },
   // https://react-svgr.com/docs/next/#usage
   webpack(config) {
     config.module.rules.push({
