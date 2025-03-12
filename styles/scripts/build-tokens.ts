88106d30103b578a9eaeccf88d63144414742e6d
kwonjuhee <77ooee@gmail.com>
2025-03-12 16:01:03 +0900

    feat: generate js token vars

--- styles/scripts/build-tokens.ts
+++ styles/scripts/build-tokens.ts
@@ -1,8 +1,9 @@
 import StyleDictionary from "style-dictionary";
+import { usesReferences } from "style-dictionary/utils";
 
 const BASE_PATH = "styles/tokens";
 
-const formatName = (path: string[]) => {
+const formatTokenName = (path: string[]) => {
   return path
     .filter((part) => part !== "DEFAULT")
     .join("-")
@@ -11,9 +12,41 @@ const formatName = (path: string[]) => {
 };
 
 StyleDictionary.registerTransform({
-  name: "name/custom-css-var",
+  name: "custom/format-token-name",
   type: "name",
-  transform: (token) => formatName(token.path),
+  transform: (token) => formatTokenName(token.path),
+});
+
+const formatReference = (value: string) => {
+  const refRegex = /\{(.+?)\}/g;
+
+  value = value.replace(refRegex, (_, pattern) => {
+    return `var(--${formatTokenName(pattern.split("."))})`;
+  });
+
+  return value;
+};
+
+StyleDictionary.registerFormat({
+  name: "custom/javascript/esm-flat",
+  format: ({ dictionary }) => {
+    return `export default ${JSON.stringify(
+      dictionary.allTokens.reduce(
+        (acc, token) => {
+          let value = token.$value;
+
+          if (usesReferences(token.original.$value)) {
+            value = formatReference(token.original.$value);
+          }
+
+          return { ...acc, [token.name]: value };
+        },
+        {} as Record<string, string>
+      ),
+      null,
+      2
+    )};`;
+  },
 });
 
 (async () => {
@@ -23,7 +56,7 @@ StyleDictionary.registerTransform({
       platforms: {
         css: {
           transformGroup: "css",
-          transforms: ["name/custom-css-var"],
+          transforms: ["custom/format-token-name"],
           buildPath: `${BASE_PATH}/base/`,
           files: [
             {
@@ -43,7 +76,7 @@ StyleDictionary.registerTransform({
       platforms: {
         css: {
           transformGroup: "css",
-          transforms: ["name/custom-css-var"],
+          transforms: ["custom/format-token-name"],
           buildPath: `${BASE_PATH}/semantic/`,
           files: [
             {
@@ -65,7 +98,7 @@ StyleDictionary.registerTransform({
       platforms: {
         css: {
           transformGroup: "css",
-          transforms: ["name/custom-css-var"],
+          transforms: ["custom/format-token-name"],
           buildPath: `${BASE_PATH}/semantic/`,
           files: [
             {
@@ -76,6 +109,67 @@ StyleDictionary.registerTransform({
                 outputReferences: true,
                 showFileHeader: false,
                 selector: `:root [data-theme="dark"]`,
+              },
+            },
+          ],
+        },
+      },
+    }),
+    new StyleDictionary({
+      source: [`${BASE_PATH}/_raw/colors.json`],
+      platforms: {
+        js: {
+          transformGroup: "js",
+          transforms: ["custom/format-token-name"],
+          buildPath: `${BASE_PATH}/vars/`,
+          files: [
+            {
+              destination: "color.ts",
+              format: "custom/javascript/esm-flat",
+              options: {
+                showFileHeader: false,
+              },
+            },
+          ],
+        },
+      },
+    }),
+    new StyleDictionary({
+      source: [`${BASE_PATH}/_raw/light-theme.json`, `${BASE_PATH}/_raw/colors.json`],
+      platforms: {
+        js: {
+          transformGroup: "js",
+          transforms: ["custom/format-token-name"],
+          buildPath: `${BASE_PATH}/vars/`,
+          files: [
+            {
+              destination: "light-theme.ts",
+              format: "custom/javascript/esm-flat",
+              filter: (token) => token.filePath === `${BASE_PATH}/_raw/light-theme.json`,
+              options: {
+                showFileHeader: false,
+              },
+            },
+          ],
+        },
+      },
+    }),
+    new StyleDictionary({
+      source: [`${BASE_PATH}/_raw/dark-theme.json`, `${BASE_PATH}/_raw/colors.json`],
+      platforms: {
+        js: {
+          transformGroup: "js",
+          transforms: ["custom/format-token-name"],
+          buildPath: `${BASE_PATH}/vars/`,
+          files: [
+            {
+              destination: "dark-theme.ts",
+              format: "custom/javascript/esm-flat",
+              filter: (token) => token.filePath === `${BASE_PATH}/_raw/dark-theme.json`,
+              options: {
+                outputReferences: true,
+                showFileHeader: false,
+                minify: true,
               },
             },
           ],
