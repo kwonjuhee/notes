87e52b986b1c6b3607bdfdb261f9558b4adc8dd1
kwonjuhee <77ooee@gmail.com>
2025-03-24 18:13:25 +0900

    fix: specify force-cache

--- api/client.ts
+++ api/client.ts
@@ -1,5 +1,16 @@
 import { Octokit } from "octokit";
 import { getEnvVar } from "@/utils/env";
 
-export const githubClient = new Octokit({ auth: getEnvVar("GITHUB_AUTH") });
+export const githubClient = new Octokit({
+  auth: getEnvVar("GITHUB_AUTH"),
+  request: {
+    fetch: (url: string, options: RequestInit): Promise<Response> => {
+      const modifiedOptions: RequestInit = {
+        ...options,
+        cache: "force-cache",
+      };
+      return fetch(url, modifiedOptions);
+    },
+  },
+});
 export type GithubClient = Octokit;
